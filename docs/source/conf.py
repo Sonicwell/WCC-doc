e7dbdddc6b7dc12f896b8329fa31c6f25162b164
menglj <menglj@astercc.com>
2025-05-07 18:11:09 +0800

    seo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,10 +25,10 @@ extensions = [
     'myst_parser',
 ]
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
+# intersphinx_mapping = {
+#     'python': ('https://docs.python.org/3/', None),
+#     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+# }
 intersphinx_disabled_domains = ['std']
 
 latex_engine = 'xelatex'
