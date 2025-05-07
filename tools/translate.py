af695a1cd87f93fe0f9655716317c912fbbf791b
menglj <menglj@astercc.com>
2025-05-07 11:19:32 +0800

    翻译脚本增加Markdown格式图片匹配

--- tools/translate.py
+++ tools/translate.py
@@ -25,13 +25,16 @@ def translate_text(text, source_lang="zh-CN", target_lang="eng"):
 # 正则匹配 <img> 标签
 img_pattern = re.compile(r'<img\s+[^>]*src=[\'"][^\'"]+[\'"][^>]*>', re.IGNORECASE)
 
+# Markdown 格式图片 ![alt text](_static/images/root/media/image1.png)
+markdown_img_pattern = re.compile(r'!\[[^\]]*\]\([^\)]+\)', re.IGNORECASE)
+
 # 读取 PO 文件
 po = polib.pofile("/usr/src/WCC-doc/docs/source/locales/en/LC_MESSAGES/client.po")
 
 # 遍历所有条目，翻译未翻译的部分
 for entry in po:
     if not entry.translated():  # 只处理未翻译的条目
-        if img_pattern.search(entry.msgid):  # 如果包含 <img>，跳过翻译
+        if img_pattern.search(entry.msgid) or markdown_img_pattern.search(entry.msgid):  # 如果是图片，跳过翻译
             entry.msgstr = ""  # 保持为空
             #TODO: 扫描图片语言目录进行替换
             print(f"跳过图片条目: {entry.msgid}")
